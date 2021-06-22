bb020e16babf626227a2e408e9f9292b80e2d3d2
shibupanda <31269265+shibupanda@users.noreply.github.com>
2021-06-22 21:02:23 +0530

    Update step.sh

--- step.sh
+++ step.sh
@@ -4,5 +4,7 @@ set -ex
 
 authentication_response="$(curl -u "$pcloudy_username:$pcloudy_access_key" https://$PCLOUDY_CLOUDURL/api/access)"
 authToken=$(echo "$authentication_response" | jq -r .result.token)
+echo $BITRISE_APK_PATH
+echo $upload_path
 upload_response="$(curl -X POST -F "file=@$upload_path" -F "source_type=raw" -F "token=$authToken" -F "filter=all" https://$PCLOUDY_CLOUDURL/api/upload_file)" 
 file_name=$(echo "$upload_response" | jq -j '.result.file' | envman add --key PCLOUDY_APP_NAME)
