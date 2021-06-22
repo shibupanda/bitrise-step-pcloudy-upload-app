ec59b5f049a0900a2928ed6b531666bcf263a605
Shibu Panda <shibu.prasad@sstsinc.com>
2021-06-22 18:20:18 +0530

    fix: syntax issue in sh file

--- step.sh
+++ step.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -ex
 
-authentication_response = "$(curl -u "$pcloudy_username:$pcloudy_access_key" https://$PCLOUDY_CLOUDURL/api/access)"
-authToken = $(echo "$authentication_response" | jq .result.token)
+authentication_response="$(curl -u "$pcloudy_username:$pcloudy_access_key" https://$PCLOUDY_CLOUDURL/api/access)"
+authToken=$(echo "$authentication_response" | jq .result.token)
 curl -X POST -F "file=@$upload_path" -F "source_type=raw" -F "token=$token" -F "filter=all" https://$PCLOUDY_CLOUDURL/api/upload_file | jq -j '.result.file' | envman add --key PCLOUDY_APP_NAME
