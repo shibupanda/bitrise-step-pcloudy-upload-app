8d40b3d5f14b94849e335f231beb11d72593a4dd
Shibu Panda <shibu.prasad@sstsinc.com>
2021-06-22 18:58:19 +0530

    fix: add -r in jq command

--- step.sh
+++ step.sh
@@ -3,5 +3,5 @@ set -ex
 
 
 authentication_response="$(curl -u "$pcloudy_username:$pcloudy_access_key" https://$PCLOUDY_CLOUDURL/api/access)"
-authToken=$(echo "$authentication_response" | jq .result.token)
+authToken=$(echo "$authentication_response" | jq -r .result.token)
 curl -X POST -F "file=@$upload_path" -F "source_type=raw" -F "token=$token" -F "filter=all" https://$PCLOUDY_CLOUDURL/api/upload_file | jq -j '.result.file' | envman add --key PCLOUDY_APP_NAME
