374fb18e2067fdb5e5fea28bfa3624a74bae11f9
Shibu Panda <shibu.prasad@sstsinc.com>
2021-06-22 19:06:22 +0530

    fix: upload response

--- step.sh
+++ step.sh
@@ -4,4 +4,5 @@ set -ex
 
 authentication_response="$(curl -u "$pcloudy_username:$pcloudy_access_key" https://$PCLOUDY_CLOUDURL/api/access)"
 authToken=$(echo "$authentication_response" | jq -r .result.token)
-curl -X POST -F "file=@$upload_path" -F "source_type=raw" -F "token=$token" -F "filter=all" https://$PCLOUDY_CLOUDURL/api/upload_file | jq -j '.result.file' | envman add --key PCLOUDY_APP_NAME
+upload_response="$(curl -X POST -F "file=@$upload_path" -F "source_type=raw" -F "token=$authToken" -F "filter=all" https://$PCLOUDY_CLOUDURL/api/upload_file)" 
+file_name=$(echo "$upload_response" | jq -j '.result.file' | envman add --key PCLOUDY_APP_NAME)
